a4f2b1e77920eaac1e41af3b4a6633f90724639a
Melvin <melvinchiem@gmail.com>
2022-10-05 15:23:39 -0700

    Added someMath() method using Commons Math library
    Out of 20 random numbers from 0-20, calculates the number of unique
    numbers and percentage of numbers less than or equal to 10.

--- Backend/TTempleProject/src/main/java/com/TeamTemple/TTempleProject/Contoller/WebController.java
+++ Backend/TTempleProject/src/main/java/com/TeamTemple/TTempleProject/Contoller/WebController.java
@@ -1,15 +1,24 @@
 package com.TeamTemple.TTempleProject.Contoller;
 
+import java.util.Random;
+
+import org.apache.commons.math3.stat.Frequency;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
 
-    @RequestMapping("/")
+    @RequestMapping("/frequency")
     public String someMath(){
-
-        return "Hello! Andy's webpage.";
+        Frequency f = new Frequency();
+        Random rand = new Random();
+        for (int i = 0; i < 20; i++) {
+            f.addValue(rand.nextInt(21));
+        }
+        int unique = f.getUniqueCount();
+        double cumFreq = f.getCumPct(10);
+        return "Unique Numbers: " + unique + "\n Percentage of numbers less than or equal to 10: " + cumFreq;
     }
 
 }
