9beee032a3be52d2de8c27b9c923ba9b79b6e087
SinHoC <89657809+SinHoC@users.noreply.github.com>
2022-10-06 23:09:57 -0700

    Update WebController.java

--- Backend/TTempleProject/src/main/java/com/TeamTemple/TTempleProject/Contoller/WebController.java
+++ Backend/TTempleProject/src/main/java/com/TeamTemple/TTempleProject/Contoller/WebController.java
@@ -16,6 +16,13 @@ import org.jsoup.*;
 import org.jsoup.nodes.*;
 import org.jsoup.select.*;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
+
 
 @RestController
 public class WebController {
@@ -64,8 +71,18 @@ public class WebController {
             }
          }
     }
+    
+    @RequestMapping("/H")
+    public class Utf8File {
+        public static String loadFileIntoString(String filePath) throws FileNotFoundException, IOException {
+            return IOUtils.toString(new FileInputStream(filePath), "UTF-8");
+        }
 
-
+        public static void saveStringIntoFile(String filePath, String content) throws IOException {
+            File f = new File(filePath);
+            FileUtils.writeStringToFile(f, content, "UTF-8");
+        }
+    }
 
 
 }
