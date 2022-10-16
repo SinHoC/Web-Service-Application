b0e1e4d8c5d550b01ac46acd79726529fdc37c9a
Melvin <melvinchiem@gmail.com>
2022-10-16 16:00:05 -0700

    Fixed syntax so backend can run

--- Backend/TTempleProject/src/main/java/com/TeamTemple/TTempleProject/Contoller/WebController.java
+++ Backend/TTempleProject/src/main/java/com/TeamTemple/TTempleProject/Contoller/WebController.java
@@ -16,19 +16,11 @@ import org.jsoup.*;
 import org.jsoup.nodes.*;
 import org.jsoup.select.*;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import org.apache.commons.io.FileUtils;
-import org.apache.commons.io.IOUtils;
-
-
 @RestController
 public class WebController {
 
     @RequestMapping("/frequency")
-    public String someMath(){
+    public String someMath() {
         Frequency f = new Frequency();
         Random rand = new Random();
         for (int i = 0; i < 20; i++) {
@@ -40,49 +32,31 @@ public class WebController {
     }
 
     @RequestMapping("/A")
-    public void someString(){
+    public void someString() {
         ImmutableList<String> immutableList = ImmutableList.of("Geeks", "For", "Geeks");
         System.out.println(immutableList);
 
-    	List<String> fruits = Lists.newArrayList("orange", "banana", "kiwi", 
+        List<String> fruits = Lists.newArrayList("orange", "banana", "kiwi",
                 "mandarin", "date", "quince");
-    	
-    	for (String fruit: fruits) {
-    		System.out.println(fruit);
 
-    	}
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+
+        }
     }
-    
-    
+
     @RequestMapping("/C")
-    public class WebController {
-    public static void main(String[] args) {
+    public void soup() {
         try {
-            String url = "http://ocw.mit.edu/courses/aeronautics-and-astronautics/16-050-thermal-energy-   fall-2002/";
+            String url = "http://ocw.mit.edu/courses/aeronautics-and-astronautics/16-050-thermal-energy-fall-2002/";
             Document doc = Jsoup.connect(url).get();
             Elements paragraphs = doc.select("p");
-        for(Element p : paragraphs)
-   System.out.println(p.text());
-
-           } 
-        catch (IOException ex) {
-          Logger.getLogger(WebController.class.getName())
-             .log(Level.SEVERE, null, ex);
+            for (Element p : paragraphs) {
+                System.out.println(p.text());
             }
-         }
-    }
-    
-    @RequestMapping("/H")
-    public class Utf8File {
-        public static String loadFileIntoString(String filePath) throws FileNotFoundException, IOException {
-            return IOUtils.toString(new FileInputStream(filePath), "UTF-8");
-        }
-
-        public static void saveStringIntoFile(String filePath, String content) throws IOException {
-            File f = new File(filePath);
-            FileUtils.writeStringToFile(f, content, "UTF-8");
+        } catch (IOException ex) {
+            Logger.getLogger(WebController.class.getName())
+                    .log(Level.SEVERE, null, ex);
         }
     }
-
-
 }
