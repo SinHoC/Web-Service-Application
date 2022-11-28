af29c87b375baf7c8b8674bddb1dbb081ded4865
Melvin <melvinchiem@gmail.com>
2022-11-28 13:06:24 -0800

    Removed files

--- Backend/TTempleProject/src/main/java/com/TeamTemple/TTempleProject/Controller/WebController.java
+++ Backend/TTempleProject/src/main/java/com/TeamTemple/TTempleProject/Controller/WebController.java
@@ -1,6 +1,5 @@
 package com.TeamTemple.TTempleProject.Controller;
 
-import org.apache.commons.math3.stat.Frequency;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,18 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.TeamTemple.TTempleProject.DBService;
 import com.TeamTemple.TTempleProject.Order;
-import com.google.common.collect.*;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.Random;
 import java.util.concurrent.ExecutionException;
-import java.io.IOException;
-import java.util.logging.*;
-import org.jsoup.*;
-import org.jsoup.nodes.*;
-import org.jsoup.select.*;
 
 @CrossOrigin
 @RestController
@@ -93,66 +83,5 @@ public class WebController {
     @GetMapping("/")
     public String getString() {
         return "Welcome to the Home Page";
-    }
-
-    @GetMapping("/frequency")
-    public double[] someMath() {
-        double[] result = new double[2];
-        Frequency f = new Frequency();
-        Random rand = new Random();
-        for (int i = 0; i < 20; i++) {
-            f.addValue(rand.nextInt(21));
-        }
-        result[0] = f.getUniqueCount();
-        result[1] = f.getCumPct(10);
-        return result;
-    }
-    
-//    @GetMapping("/order1")
-//    public List<Order> order1() {
-//        Order  order = new Order("Melvin Chiem-Ngoy", "123-456-7890");
-//        Order  order2 = new Order("Yu Sun", "123-123-1234");
-//        Order  order3 = new Order("D'Brickashaw Ferguson", "109-876-5432");
-//        
-//        List<Order> list = new ArrayList<Order>();
-//        list.add(order);
-//        list.add(order2);
-//        list.add(order3);
-//        
-//        return list;
-//    }
-
-    @RequestMapping("/A")
-    public int multiply(int a, int b) {
-        return a*b;
-
-    }
-    
-        @RequestMapping("/Subtraction")
-    public int subtract(int a, int b) {
-        return a-b;
-
-    }
-    
-    @RequestMapping("/Calculator")
-    public int Calculator(int a, int b) {
-        return a+b;
-
-    }
-    
-
-    @RequestMapping("/C")
-    public void soup() {
-        try {
-            String url = "http://ocw.mit.edu/courses/aeronautics-and-astronautics/16-050-thermal-energy-fall-2002/";
-            Document doc = Jsoup.connect(url).get();
-            Elements paragraphs = doc.select("p");
-            for (Element p : paragraphs) {
-                System.out.println(p.text());
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(WebController.class.getName())
-                    .log(Level.SEVERE, null, ex);
-        }
     }
 }
